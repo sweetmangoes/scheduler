c1f8d1f41d5f38e5167035873a68d0c9a4419dc8
sweetmangoes <alphonsechristian@gmail.com>
2023-01-31 17:33:39 -0500

    created Appointment folder within compements, setup style sheet and index

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -14,8 +14,8 @@ export default function InterviewerList(props){
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === value}
-        setInterviewer={() => onChange(interviewer.id)}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
     )
   });
