b11fd2bd25697084ba0736c0ba455191f063e8ff
sweetmangoes <alphonsechristian@gmail.com>
2023-01-30 21:20:25 -0500

    built InterviewerList component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,12 +3,10 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem(props){
-  console.log(props.name)
 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": props.selected
   })
-
 
 
   return (
