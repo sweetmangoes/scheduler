7419531ed50a1d2d2814c42462c799ed88a90750
sweetmangoes <alphonsechristian@gmail.com>
2023-02-03 17:28:42 -0500

    deleted days array, using useState to store days and axios to fetch days data

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,24 +5,6 @@ import axios from "axios";
 import "components/Appointment"; 
 import Appointment from "components/Appointment";
 
-// Daylist
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -64,7 +46,15 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(()=> {
+    const daysURL = `http://localhost:8001/api/days`
+    axios.get(daysURL).then(response => {
+      console.log(response.data)
+      setDays(response.data)
+    })
+  }, [])
 
   const appointmentList = Object.values(appointments).map(
     (appointment) => {
@@ -89,8 +79,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            value={day}
-            onChange={setDay}
+            value={days}
+            onChange={setDays}
           />          
         </nav>
         <img
