715fba6c466cd6d0532aa3aa3b5ce9ce360ee307
sweetmangoes <alphonsechristian@gmail.com>
2023-02-08 20:49:15 -0500

    refactored code to add interviewer name, created helperfunction to getInterview obj

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import DayList from "./DayList";
 import axios from "axios";
 import "components/Appointment"; 
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -14,27 +14,28 @@ export default function Application(props) {
     appointments: {}
   });
 
-  const dailyAppointments =getAppointmentsForDay(state, state.day); 
+  const appointments =getAppointmentsForDay(state, state.day); 
   const setDay = day => setState({ ...state, day });
+  const appointmentList = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />)
+  })
 
+  // fetching API data
   useEffect(()=> {
     Promise.all([
       axios.get(`http://localhost:8001/api/days`),
       axios.get(`http://localhost:8001/api/appointments`),
+      axios.get(`http://localhost:8001/api/interviewers`),
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
-    })
-  }, [])
-
-  const appointmentList = dailyAppointments.map(
-    (appointment) => {
-      return (
-        <Appointment 
-        key={appointment.id}
-        {... appointment}
-      />
-      )
-    });
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers:all[2].data }))
+    }, [])})
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,17 @@ export function getAppointmentsForDay(state, day) {
   }
   return answer; 
 }
+
+export function getInterview(state, interview) {
+  if (!interview){
+    return null
+  }; 
+  const interviewID =  interview.interviewer;
+  const interviewersID = state.interviewers[interviewID].id; 
+  if(interviewersID === interviewID){
+    const interviewObj = state.interviewers[interviewID];
+    interview.interviewer = interviewObj; 
+    return interview; 
+  }
+  return null; 
+}
